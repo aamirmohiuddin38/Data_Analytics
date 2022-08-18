57b0803d8422022fa61335dddc942a29255bd857
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-08-18 22:26:52 +0530

    converting different date format

--- SQL practice/dates.sql
+++ SQL practice/dates.sql
@@ -3,31 +3,32 @@
 -- use date_db;
 
 -- TODO: CREATE DUMMY TABLE
--- create table test_table(`Date` varchar(20))
+-- create table test_table(Date_New varchar(20))
 -- Insert into test_table values
---     ('01-07-2022'),
---     ('22-05-2021'),
---     ('15-04-1997'),
---     ('03-04-1996')
+--     ('2021-Aug-02'),
+--     ('2021-Jul-03'),
+--     ('2021-Jun-04'),
+--     ('2021-Apr-05')
 
 -- select * from test_table;
 
 -- till now we have just created a table and inserted some of the dummy dates as varchar. Lets check check can we retrieve some of the information as dates from these values
 
 -- TODO: Let's get year from these values
--- SELECT 'Date', year('date') from test_table;  // We get the values as NULL because we applied date function on varchar. So we have to convert the datatype of this column to date:
+-- SELECT Date_new, year('Date_New') from test_table; 
+--  // We get the values as NULL because we applied date function on varchar. So we have to convert the datatype of this column to date:
 
 -- DESC test_table;
 
 -- TODO: Convert datatype from varchar to date 
 
 -- UPDATE test_table
--- set `Date` = str_to_date(`Date`, '%d-%m-%Y')
+-- set Date_New = str_to_date(Date_New, '%Y-%b-%d')
 
 -- ALTER TABLE test_table
--- MODIFY column `Date` date
+-- MODIFY column Date_New date
 
--- select * from test_table;
+-- select Date_New,Date_Format(Date_New, '%d/%b/%Y') from test_table;
 -- DESC test_table
 
 -- INSERT INTO test_table VALUES('2001-01-01')
