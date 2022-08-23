4d7601aff68b0986be6fdec185a9cb3292d2e9b1
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-08-23 12:57:26 +0530

    ROWS clause with LAST_VALUE()

--- SQL practice/windowFun.sql
+++ SQL practice/windowFun.sql
@@ -454,4 +454,67 @@ but this query has problem if there two students with same marks as in fsbc batc
 +------------+---------------+--------------+----------------+----------------+-------------------+-------------+
 */
 
--- SELECT * , LAST_VALUE(students_marks) OVER(ORDER BY students_marks DESC) as "Last value" FROM ineuron_students;
+-- SELECT * , LAST_VALUE(students_marks) OVER(ORDER BY students_marks desc ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as "Last value" FROM ineuron_students;
+-- OUTPUT:
+/*
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+| student_id | student_batch | student_name | student_stream | students_marks | student_mail_id   | Last value |
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+|        108 | fsds          | snehal       | CI             |             89 | snehal@gmail.com  |         23 |
+|        115 | fsde          | prateek      | EE             |             89 | prateek@gmail.com |         23 |
+|        104 | fsda          | sanket       | cs             |             82 | sanket@gmail.com  |         23 |
+|        102 | fsda          | sanket       | cs             |             81 | sanket@gmail.com  |         23 |
+|        100 | fsda          | saurabh      | cs             |             80 | saurabh@gmail.com |         23 |
+|        103 | fsda          | shyam        | cs             |             80 | shyam@gmail.com   |         23 |
+|        106 | fsds          | ajay         | ME             |             78 | ajay@gmail.com    |         23 |
+|        105 | fsda          | shyam        | ME             |             67 | shyam@gmail.com   |         23 |
+|        112 | fsde          | mohit        | EE             |             67 | mohit@gmail.com   |         23 |
+|        119 | fsbc          | sandeep      | ECE            |             65 | sandeep@gmail.com |         23 |
+|        119 | fsbc          | saurabh      | ECE            |             65 | saurabh@gmail.com |         23 |
+|        106 | fsds          | ajay         | ME             |             45 | ajay@gmail.com    |         23 |
+|        110 | fsds          | rakesh       | CI             |             45 | rakesh@gmail.com  |         23 |
+|        114 | fsde          | gaurav       | EE             |             45 | gaurav@gmail.com  |         23 |
+|        118 | fsbc          | pranay       | ECE            |             45 | pranay@gmail.com  |         23 |
+|        111 | fsde          | anuj         | CI             |             43 | anuj@gmail.com    |         23 |
+|        109 | fsds          | manisha      | CI             |             34 | manisha@gmail.com |         23 |
+|        113 | fsde          | vivek        | EE             |             23 | vivek@gmail.com   |         23 |
+|        116 | fsde          | mithun       | ECE            |             23 | mithun@gmail.com  |         23 |
+|        117 | fsbc          | chaitra      | ECE            |             23 | chaitra@gmail.com |         23 |
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+*/
+
+
+-- SELECT * , LAST_VALUE(students_marks) 
+-- OVER(
+--     PARTITION BY student_batch
+--     ORDER BY students_marks desc
+--     ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+--     ) AS "Last value" FROM ineuron_students;
+
+-- OUTPUT:
+/*
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+| student_id | student_batch | student_name | student_stream | students_marks | student_mail_id   | Last value |
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+|        119 | fsbc          | sandeep      | ECE            |             65 | sandeep@gmail.com |         23 |
+|        119 | fsbc          | saurabh      | ECE            |             65 | saurabh@gmail.com |         23 |
+|        118 | fsbc          | pranay       | ECE            |             45 | pranay@gmail.com  |         23 |
+|        117 | fsbc          | chaitra      | ECE            |             23 | chaitra@gmail.com |         23 |
+|        104 | fsda          | sanket       | cs             |             82 | sanket@gmail.com  |         67 |
+|        102 | fsda          | sanket       | cs             |             81 | sanket@gmail.com  |         67 |
+|        100 | fsda          | saurabh      | cs             |             80 | saurabh@gmail.com |         67 |
+|        103 | fsda          | shyam        | cs             |             80 | shyam@gmail.com   |         67 |
+|        105 | fsda          | shyam        | ME             |             67 | shyam@gmail.com   |         67 |
+|        115 | fsde          | prateek      | EE             |             89 | prateek@gmail.com |         23 |
+|        112 | fsde          | mohit        | EE             |             67 | mohit@gmail.com   |         23 |
+|        114 | fsde          | gaurav       | EE             |             45 | gaurav@gmail.com  |         23 |
+|        111 | fsde          | anuj         | CI             |             43 | anuj@gmail.com    |         23 |
+|        113 | fsde          | vivek        | EE             |             23 | vivek@gmail.com   |         23 |
+|        116 | fsde          | mithun       | ECE            |             23 | mithun@gmail.com  |         23 |
+|        108 | fsds          | snehal       | CI             |             89 | snehal@gmail.com  |         34 |
+|        106 | fsds          | ajay         | ME             |             78 | ajay@gmail.com    |         34 |
+|        106 | fsds          | ajay         | ME             |             45 | ajay@gmail.com    |         34 |
+|        110 | fsds          | rakesh       | CI             |             45 | rakesh@gmail.com  |         34 |
+|        109 | fsds          | manisha      | CI             |             34 | manisha@gmail.com |         34 |
++------------+---------------+--------------+----------------+----------------+-------------------+------------+
+*/
