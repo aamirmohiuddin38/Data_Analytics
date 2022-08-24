b28d0c608a25fe4f1f5b4336ebd9b48c4237d97a
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-08-24 14:17:22 +0530

    RANGE partiioning

--- SQL practice/partitioning.sql
+++ SQL practice/partitioning.sql
@@ -54,4 +54,59 @@
 +------------------+-----------+--------------+----------------------+-------------+------------+---------------+-------------------+
 */
 
-select * from ineuron_course where course_lauch_year = 2020
+-- select * from ineuron_course where course_lauch_year = 2020
+/* What above query does is it will check all the rows of a table for given condition and will return the resulting records. So this query will take longer time for large data table.
+Now what we can do is we can create new table where in we can store the data year wise, so when we look for data of some particular year, only that chunk or portion of data is examined,
+which will optimise our query execution -> for this PARTITIONING COMES INTO PICTURE */
+
+-- TODO: TYPES OF PARTITIONING
+-- TODO: "RANGE" PARTITIONING:
+
+-- create table ineuron_courses1(
+-- course_name varchar(50),
+-- course_id int,
+-- course_title varchar(60),
+-- course_desc varchar(80),
+-- launch_date date,
+-- course_fee int,
+-- course_mentor varchar(60),
+-- course_launch_year int)
+-- PARTITION BY RANGE(course_launch_year)(
+-- PARTITION p0 VALUES less than (2019),
+-- PARTITION p1 VALUES less than (2020),
+-- PARTITION p2 VALUES less than (2021),
+-- PARTITION p3 VALUES less than (2022),
+-- PARTITION p4 VALUES less than(2023));
+
+-- insert into ineuron_courses1 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+
+-- ('aiops' , 101 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('dlcvnlp' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('aws cloud' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('blockchain' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('RL' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('Dl' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('interview prep' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('big data' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('data analytics' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fsds' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('fsda' , 101 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fabe' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('java' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('MERN' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) 
+
+-- select * from ineuron_courses1 where course_launch_year = 2020
+
+-- select partition_name, table_name, table_rows from information_schema.partitions where table_name = 'ineuron_courses1'
+-- output:
+/*
++----------------+------------------+------------+
+| PARTITION_NAME | TABLE_NAME       | TABLE_ROWS |
++----------------+------------------+------------+
+| p0             | ineuron_courses1 |          0 |
+| p1             | ineuron_courses1 |          4 |
+| p2             | ineuron_courses1 |          4 |
+| p3             | ineuron_courses1 |          3 |
+| p4             | ineuron_courses1 |          4 |
++----------------+------------------+------------+
+*/
