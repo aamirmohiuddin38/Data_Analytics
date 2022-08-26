0f030e287767100c61f6ef4b1645d34fed6983cf
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-08-27 00:07:39 +0530

    LIST partitioning

--- SQL practice/partitioning.sql
+++ SQL practice/partitioning.sql
@@ -170,3 +170,115 @@ you most likely want to follow this with PARTITIONS num, where num is a positive
 -- ('fabe' , 101 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
 -- ('java' , 101 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
 -- ('MERN' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) 
+
+
+-- TODO: "KEY" PARTITION
+/*
+Partitioning by key is similar to partitioning by hash, except that where hash partitioning employs a user-defined expression,
+the hashing function for key partitioning is supplied by the MySQL server.
+*/
+
+-- create table ineuron_courses4(
+-- course_name varchar(50),
+-- course_id int(10) primary key ,
+-- course_title varchar(60),
+-- course_desc varchar(80),
+-- launch_date date,
+-- course_fee int,
+-- course_mentor varchar(60),
+-- course_launch_year int)
+-- partition by key()
+-- partitions 10;
+
+
+
+-- select partition_name , table_name , table_rows from information_schema.partitions where table_name = 'ineuron_courses4'
+
+
+-- insert into ineuron_courses4 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+
+-- ('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019)
+
+-- select * from ineuron_courses4 
+
+-- SELECT MD5('MERN')
+
+-- TODO: "LIST" Partitioning
+/*
+List partitioning in MySQL is similar to range partitioning in many ways. As in partitioning by RANGE, each partition must be explicitly defined. 
+The chief difference between the two types of partitioning is that, in list partitioning, each partition is defined and selected based on the 
+membership of a column value in one of a set of value lists, rather than in one of a set of contiguous ranges of values. 
+This is done by using PARTITION BY LIST(expr) where expr is a column value or an expression based on a column value and returning an integer 
+value, and then defining each partition by means of a VALUES IN (value_list), where value_list is a comma-separated list of integers.
+*/
+
+-- create table ineuron_courses6(
+-- course_name varchar(50) ,
+-- course_id int(10) ,
+-- course_title varchar(60),
+-- course_desc varchar(80),
+-- launch_date date,
+-- course_fee int,
+-- course_mentor varchar(60),
+-- course_launch_year int)
+-- partition by list(course_launch_year)(
+-- partition p0 values in(2019,2020),
+-- partition p1 values in(2022,2021)
+-- )
+
+-- insert into ineuron_courses6 values('machine_learning' , 101 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+
+-- ('aiops' , 102 , 'ML', "this is aiops course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('dlcvnlp' , 103 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('aws cloud' , 104 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('blockchain' , 105, 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('RL' , 106 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('Dl' , 107 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('interview prep' , 108 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019) ,
+-- ('big data' , 109 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('data analytics' , 110 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fsds' , 1011 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('fsda' , 1012 , 'ML', "this is ML course" ,'2021-07-07',3540,'sudhanshu',2021) ,
+-- ('fabe' , 1013 , 'ML', "this is ML course" ,'2022-07-07',3540,'sudhanshu',2022) ,
+-- ('java' , 1014 , 'ML', "this is ML course" ,'2020-07-07',3540,'sudhanshu',2020) ,
+-- ('MERN' , 1015 , 'ML', "this is ML course" ,'2019-07-07',3540,'sudhanshu',2019)
+
+-- select partition_name , table_name , table_rows from information_schema.partitions where table_name = 'ineuron_courses6'
+
+-- TODO: ANOTHER EXAMPLE OF LIST PARTITIONING
+/*
+CREATE TABLE employees (
+    id INT NOT NULL,
+    fname VARCHAR(30),
+    lname VARCHAR(30),
+    hired DATE NOT NULL DEFAULT '1970-01-01',
+    separated DATE NOT NULL DEFAULT '9999-12-31',
+    job_code INT,
+    store_id INT
+)
+PARTITION BY LIST(store_id) (
+    PARTITION pNorth VALUES IN (3,5,6,9,17),
+    PARTITION pEast VALUES IN (1,2,10,11,19,20),
+    PARTITION pWest VALUES IN (4,12,13,14,18),
+    PARTITION pCentral VALUES IN (7,8,15,16)
+);
+This makes it easy to add or drop employee records relating to specific regions to or from the table. For instance, suppose that all stores in 
+the West region are sold to another company. In MySQL 5.6, all rows relating to employees working at stores in that region can be deleted 
+with the query ALTER TABLE employees TRUNCATE PARTITION pWest, which can be executed much more efficiently than the equivalent DELETE 
+statement DELETE FROM employees WHERE store_id IN (4,12,13,14,18);. (Using ALTER TABLE employees DROP PARTITION pWest would also delete 
+all of these rows, but would also remove the partition pWest from the definition of the table; you would need to use an ALTER TABLE ... ADD 
+PARTITION statement to restore the table's original partitioning scheme.)
+*/
