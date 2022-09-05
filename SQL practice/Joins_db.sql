af3244d20df49ab6cda846114b66043393c7f2b6
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-09-05 16:30:03 +0530

    Left Join

--- SQL practice/Joins_db.sql
+++ SQL practice/Joins_db.sql
@@ -5,7 +5,7 @@ SELECT O.OrderID, C.CustomerName, O.OrderDate
     -> INNER JOIN Customers C
     -> ON O.CustomerID = C.CustomerID);
 
-
+--TODO: Q.2: Get me order_id, customer name and who is the shipper for that order.
     SELECT O.OrderID, C.CustomerName, S.ShipperName
     -> FROM ((Orders O
     -> INNER JOIN Customers C
@@ -13,3 +13,9 @@ SELECT O.OrderID, C.CustomerName, O.OrderDate
     -> INNER JOIN Shippers S
     -> ON O.ShipperID = S.ShipperID) 
     -> LIMIT 5;
+
+--TODO: Q.3: Get me all customers name and their order id.
+    SELECT C.CustomerName, O.OrderID
+    -> FROM (Customers C
+    -> LEFT JOIN Orders O
+    -> ON C.CustomerID = O.CustomerID);
