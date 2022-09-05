7337253f3afacd587ffd51a19434e3f67587c218
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-09-05 15:57:18 +0530

    Inner Join on 3 tables

--- SQL practice/Joins_db.sql
+++ SQL practice/Joins_db.sql
@@ -1,6 +1,15 @@
 --TODO: Q.1: Get me all order id, customer name and date when order was placed.
 
 SELECT O.OrderID, C.CustomerName, O.OrderDate
-    -> from Orders O
+    -> from (Orders O
     -> INNER JOIN Customers C
-    -> ON O.CustomerID = C.CustomerID;
+    -> ON O.CustomerID = C.CustomerID);
+
+
+    SELECT O.OrderID, C.CustomerName, S.ShipperName
+    -> FROM ((Orders O
+    -> INNER JOIN Customers C
+    -> ON O.CustomerID = C.CustomerID)
+    -> INNER JOIN Shippers S
+    -> ON O.ShipperID = S.ShipperID) 
+    -> LIMIT 5;
