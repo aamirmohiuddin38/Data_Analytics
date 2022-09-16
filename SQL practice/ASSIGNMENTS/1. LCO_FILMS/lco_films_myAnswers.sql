285234c133bfeb936029def15702791f89c40479
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-09-17 00:39:59 +0530

    Answer 2

--- SQL practice/ASSIGNMENTS/1. LCO_FILMS/lco_films_myAnswers.sql
+++ SQL practice/ASSIGNMENTS/1. LCO_FILMS/lco_films_myAnswers.sql
@@ -1,8 +1,27 @@
 -- Q.1: Which categories of movies released in 2018? Fetch with the number of movies. 
 
+/*
 SELECT category.name, film.release_year, COUNT(category.category_id) AS number_of_films 
     FROM category 
     LEFT JOIN film_category ON film_category.category_id = category.category_id 
     LEFT JOIN film ON  film_category.film_id =  film.film_id
     WHERE film.release_year = "2018" 
     GROUP BY category.category_id;
+*/
+
+/*
+-- Q.2:  Update the address of actor id 36 to “677 Jazz Street”. 
+UPDATE `address` 
+    INNER JOIN actor ON actor.address_id=address.address_id 
+    SET `address`="677 Jazz Street"
+WHERE actor.actor_id=36
+
+            OR
+
+UPDATE address
+    SET address = "677 Jazz Street"
+    WHERE
+        (SELECT address_id FROM actor
+            WHERE actor_id = 36);
+
+*/
