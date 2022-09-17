29d817bf8b98aa47f462f7f35abcbf5d50ecbe95
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-09-17 15:31:09 +0530

    Answer-4

--- SQL practice/ASSIGNMENTS/1. LCO_FILMS/lco_films_myAnswers.sql
+++ SQL practice/ASSIGNMENTS/1. LCO_FILMS/lco_films_myAnswers.sql
@@ -1,15 +1,14 @@
 -- Q.1: Which categories of movies released in 2018? Fetch with the number of movies. 
 
-/*
 SELECT category.name, film.release_year, COUNT(category.category_id) AS number_of_films 
     FROM category 
     LEFT JOIN film_category ON film_category.category_id = category.category_id 
     LEFT JOIN film ON  film_category.film_id =  film.film_id
     WHERE film.release_year = "2018" 
     GROUP BY category.category_id;
-*/
 
-/*
+
+
 -- Q.2:  Update the address of actor id 36 to “677 Jazz Street”. 
 UPDATE `address` 
     INNER JOIN actor ON actor.address_id=address.address_id 
@@ -24,9 +23,9 @@ UPDATE address
         (SELECT address_id FROM actor
             WHERE actor_id = 36);
 
-*/
 
-/*
+
+
 -- Q.3: Add the new actors (id : 105 , 95) in film  ARSENIC INDEPENDENCE (id:41).
 INSERT INTO film_actor (actor_id, film_id)
         VALUES (105, 41),
@@ -35,4 +34,17 @@ INSERT INTO film_actor (actor_id, film_id)
     actor_id = VALUES(actor_id),
     film_id = VALUES(film_id);
 
-    /*
+    
+
+
+-- Q.4:  Get the name of films of the actors who belong to India.
+
+SELECT DISTINCT film.title , country.country
+    FROM film INNER JOIN film_actor ON film.film_id =  film_actor.film_id 
+    INNER JOIN actor ON actor.actor_id = film_actor.actor_id 
+    INNER JOIN address ON address.address_id = actor.address_id 
+    INNER JOIN city ON city.city_id = address.city_id 
+    INNER JOIN country ON country.country_id=city.country_id 
+    WHERE country.country = "India"; 
+
+-- Q.5: 
