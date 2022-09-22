9d4acb95bef60c1fbb353abc05abc31d7ef9635c
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-09-22 21:55:19 +0530

    Answer-1: Insert cmd

---
+++ SQL practice/ASSIGNMENTS/2. CAR RENTALS/car_rentals_myAnswers.sql
@@ -0,0 +1,9 @@
+-- Q.1: Insert the details of new customer:- 
+    -- First name : Nancy 
+    -- Last Name: Perry 
+    -- Dob : 1988-05-16 
+    -- License Number: K59042656E 
+    -- Email : nancy@gmail.com 
+
+INSERT INTO customer(first_name, last_name, dob, driver_license_number, email)
+        VALUES('Nancy', 'Perry', '1988-05-06', 'K59042656E', 'nancy@gmail.com');
